aa666d9d58d26483b8c8d8a134144c2aeee30ba6
Kazeke <kazbek_kaddirov@mail.ru>
2019-01-07 16:45:51 +0500

    Lesson1

--- src/com/javalessons/HelloWorld1.java
+++ src/com/javalessons/HelloWorld1.java
@@ -1,4 +1,4 @@
-package com.lesson1;
+package com.javalessons;
 
 public class HelloWorld1 {
     public static void main(String[] args) {
---
+++ src/com/javalessons/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.javalessons.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
