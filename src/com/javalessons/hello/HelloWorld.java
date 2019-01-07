64f68282ce8a1d431553db43741cdc3d48354570
Kazeke <kazbek_kaddirov@mail.ru>
2019-01-07 17:08:30 +0500

    Урок переменные 1 Lesson2

--- src/com/javalessons/hello/HelloWorld.java
+++ src/com/javalessons/hello/HelloWorld.java
@@ -3,6 +3,19 @@ package com.javalessons.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        byte b =10;
+        short c=200;
+        int a = b + 23;
+        long myLongValue = 456789L;
+
+        float myFloatValue=23.36f;
+        double myDoubleValue=56.78d;
+
+        System.out.println("byte b = " + b);
+        System.out.println("short c = " + c);
+        System.out.println("int a = " + a);
+
     }
 
 
